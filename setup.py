6961449aa2f596187715931eacc0480d77fd20d3
Alex Schwarz <alex.schwarz@informatik.tu-chemnitz.de>
2020-02-26 16:48:20 +0100

    Add a Desktop-Shortcut on Windows machines

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 """ Setup Module """
+import os
+import sys
 from setuptools import setup, find_packages
 from ArrayViewer import __version__
+if os.name == 'nt':
+    try:
+        from win32com.client import Dispatch
+        from win32com.shell import shell
+        from win32com.shell.shellcon import CSIDL_DESKTOP
+        winload = True
+    except:
+        winload = False
 
 PACKAGE_NAME = "ArrayViewer"
 
@@ -9,6 +19,29 @@ with open('requirements.txt') as req_file:
 with open("README.md", "r") as readme_file:
     LONG_DESC = readme_file.read()
 
+def post_install():
+    """ Windows post install function. """
+    try:
+        desktop = shell.SHGetSpecialFolderPath(0, CSIDL_DESKTOP)
+        prefix = sys.prefix
+        if '--user' in sys.argv:
+            siteP = [n for n in sys.path
+                     if 'site-packages' in os.path.split(n)[1]]
+            for p in siteP:
+                prefix = os.path.split(p)[0]
+                if os.path.isfile(os.path.join(prefix,
+                                               "Scripts", "aview.exe")):
+                    break
+        script = os.path.join(prefix, "Scripts", "aview.exe")
+        if not os.path.isfile(script):
+            raise FileNotFoundError
+        dpt = Dispatch('WScript.Shell')
+        sh = dpt.CreateShortcut(os.path.join(desktop, "ArrayViewer.lnk"))
+        sh.Targetpath = script
+        sh.WorkingDirectory = os.path.split(script)[0]
+        sh.save()
+    except:
+        print("No shortcut created!")
 
 if __name__ == "__main__":
     setup(
@@ -28,3 +61,5 @@ if __name__ == "__main__":
         long_description=LONG_DESC,
         long_description_content_type="text/markdown",
     )
+    if os.name == "nt" and winload and 'install' in sys.argv:
+        post_install()
